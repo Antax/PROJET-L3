2d30851f563a51d210e4d4618e60705a52807083
enzo-cos <enzo.costantini2@gmail.com>
2021-03-27 00:16:17 +0100

    test

--- Automaton.cc
+++ Automaton.cc
@@ -763,8 +763,8 @@ int main(int argc, char **argv){
       nbStates=stoi(argv[3]);
     }
     //printf("nb : %d\n",nbStates);
-    srand(20);
-    //fa::Automaton A1=RandomAutomaton(40);
+    srand(25);
+    fa::Automaton A1=RandomAutomaton(20);
     if(argc==5){
       srand(atoi(argv[4]));
     }else{
@@ -772,19 +772,19 @@ int main(int argc, char **argv){
     }
       
   //Automate reconnaissant tous les mots
-       fa::Automaton A1;
+      //  fa::Automaton A1;
 
-      A1.addSymbol('a');
-      A1.addSymbol('b');
-      A1.addState(0);
+      // A1.addSymbol('a');
+      // A1.addSymbol('b');
+      // A1.addState(0);
 
-      A1.setStateInitial(0);
-      A1.setStateFinal(0);
+      // A1.setStateInitial(0);
+      // A1.setStateFinal(0);
 
-       A1.addTransition(0,'a',0);
-       A1.addTransition(0,'b',0);
-      A1.addState(0);A1.addState(1);
-      A1.addState(2);A1.addState(3);
+      //  A1.addTransition(0,'a',0);
+      //  A1.addTransition(0,'b',0);
+      // A1.addState(0);A1.addState(1);
+      // A1.addState(2);A1.addState(3);
       
       fa::Automaton A2=RandomAutomaton(nbStates);
         //A1.dotPrint(std::cout);
--- TestsAutomaton.cc
+++ TestsAutomaton.cc
@@ -940,21 +940,21 @@ fa::Automaton RandomAutomaton(int nbstates){
 
 int main(int argc, char **argv){
   srand(20);
-  //fa::Automaton A1=RandomAutomaton(40);
+  fa::Automaton A1=RandomAutomaton(20);
 
-       fa::Automaton A1;
-      A1.addSymbol('a');
-      A1.addSymbol('b');
+      //  fa::Automaton A1;
+      // A1.addSymbol('a');
+      // A1.addSymbol('b');
 
-      A1.addState(0);
+      // A1.addState(0);
 
-      A1.setStateInitial(0);
-      A1.setStateFinal(0);
+      // A1.setStateInitial(0);
+      // A1.setStateFinal(0);
 
-      A1.addTransition(0,'a',0);
-      A1.addTransition(0,'b',0);
-      A1.addState(0);A1.addState(1);
-      A1.addState(2);A1.addState(3);
+      // A1.addTransition(0,'a',0);
+      // A1.addTransition(0,'b',0);
+      // A1.addState(0);A1.addState(1);
+      // A1.addState(2);A1.addState(3);
 
       if(argc>2){
       srand(atoi(argv[2]));
